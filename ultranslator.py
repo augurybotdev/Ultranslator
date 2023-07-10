12bc75a67af8296d71bf1ce5325931e9282aea7e
augurybotdev <136403737+augurybotdev@users.noreply.github.com>
2023-07-10 04:38:47 -0400

    Update ultranslator.py
    Interface alteration - remove sidebar. Implement expanders for translation storage histories. Add select-box feature with all prior translation languages saved for easy re-selection

--- ultranslator.py
+++ ultranslator.py
@@ -20,12 +20,18 @@ def get_completion(prompt, model="gpt-4-0613"):
         temperature=0, 
     )
     return response.choices[0].message["content"]
-
-
 text = st.text_area("enter text to translate")
-style = st.text_area("enter translation directions")
-# style2 = st.text_area("enter another set of translation directions")
 
+if "saved_styles" not in st.session_state:
+    st.session_state.saved_styles = []
+
+# Display styles in a dropdown
+selected_style = st.selectbox('Your saved languages and style directions', [''] + st.session_state.saved_styles)
+style = st.text_area("enter translation directions", value=selected_style)
+
+if style and style not in st.session_state.saved_styles:
+    st.session_state.saved_styles.append(style)
+    
 st.divider()
 
 template_string = """Translate the text \
@@ -37,6 +43,8 @@ translate that reference to "horse and carriage". \
 text: ```{text}```
 """
 
+
+
 chat = ChatOpenAI(temperature=0.0)
 prompt_template = ChatPromptTemplate.from_template(template_string)
 
@@ -45,48 +53,29 @@ formatted_text = prompt_template.format_messages(
     text=text
 )
 
-# formatted_text2 = prompt_template.format_messages(
-#     style=style2,
-#     text=text
-# )
-
 if "text" not in st.session_state:
     st.session_state.text = []
 if "style" not in st.session_state:
     st.session_state.style = []
-if "style2" not in st.session_state:
-    st.session_state.style2 = []
 if "responses" not in st.session_state:
     st.session_state.responses = []
-# if "responses2" not in st.session_state:
-#     st.session_state.responses2 = []
+
 
 if text:
-    if style:
+    if style:     
         st.session_state.text.append(text)
         st.session_state.style.append(style)
         
         translated_response = chat(formatted_text)
         response = translated_response.content
         st.session_state.responses.append(response)
-        # st.markdown(response, help="your translated text")
-        
-        # if style2:
-        #     st.session_state.style2.append(style2)
 
-        #     translated_response2 = chat(formatted_text2)
-        #     response2 = translated_response2.content
-        #     st.session_state.responses2.append(response2)
-        #     st.markdown(response2, help="your translated text")
-        
-        
-        
-        with st.sidebar:
-            st.subheader("History")
-            for i in range(len(st.session_state.responses)):
-                st.text(st.session_state.text[i])
-                st.text(st.session_state.style[i])
-                st.text(st.session_state.responses[i])
-                # st.write(st.session_state.style2[i])
-                # st.write(st.session_state.responses2[i])
-                st.divider()
+        for i in range(len(st.session_state.responses)):
+            with st.expander(f'{st.session_state.style[i]}', expanded=False):
+                st.text('FROM:')
+                st.write(st.session_state.text[i])
+                st.text('TO:')
+                st.write(st.session_state.style[i])
+                st.text('TRANSLATION:')
+                response_text = st.session_state.responses[i].replace('```', '')
+                st.write(response_text)  
